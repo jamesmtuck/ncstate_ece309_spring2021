5d64ac2d3539774ef9731fd8e74d6be8d23bdbaf
James Tuck <jtuck@ncsu.edu>
2021-03-02 13:52:10 -0500

    office hours week 7

---
+++ week7/officehours/inheritance.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+class Base_Class
+{
+protected:
+  int x;
+public:
+  virtual int get() { return x; }
+};
+
+// Derived_Class is a Base_Class
+class Derived_Class : public Base_Class
+{
+public:
+  int y;
+  Derived_Class(){
+    x = 10;
+  }
+  virtual int get() {
+    std::cout << "called Derived_Class::get()" << std::endl;
+    return y;
+  }
+};
+
+
+
+int main()
+{
+  //Derived_Class * d = new Base_Class();
+  //d->y = 3;
+  
+  Derived_Class *d = new Derived_Class();
+  Base_Class *b = d;
+  //b->x = 5;
+  //d->x = 6;
+  d->y = 7;
+  std::cout << b->get() << std::endl;
+  
+  std::cout << " " << d->y << std::endl;
+  
+  return 0;
+}
